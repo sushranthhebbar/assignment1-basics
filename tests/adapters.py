af2bb30c90dac462fbbb0b487abf45cf28cae70e
Sushranth Hebbar <sushranthhebbar@gmail.com>
2026-02-08 17:30:12 +0530

    Added SwiGLU

--- tests/adapters.py
+++ tests/adapters.py
@@ -82,14 +82,14 @@ def run_swiglu(
     Returns:
         Float[Tensor, "... d_model"]: Output embeddings of the same shape as the input embeddings.
     """
-    # Example:
-    # If your state dict keys match, you can use `load_state_dict()`
-    # swiglu.load_state_dict(weights)
-    # You can also manually assign the weights
-    # swiglu.w1.weight.data = w1_weight
-    # swiglu.w2.weight.data = w2_weight
-    # swiglu.w3.weight.data = w3_weight
-    raise NotImplementedError
+    from cs336_basics.SwiGLU import SwiGLU
+    swiglu = SwiGLU(d_model, d_ff)
+    swiglu.load_state_dict({
+        "W1": w1_weight.T.contiguous(),
+        "W2": w2_weight.T.contiguous(),
+        "W3": w3_weight.T.contiguous(),
+    })
+    return swiglu(in_features)
 
 
 def run_scaled_dot_product_attention(
