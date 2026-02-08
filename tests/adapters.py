bedd01e5ef047ef6923fe92b4717113201a51d0d
Sushranth Hebbar <sushranthhebbar@gmail.com>
2026-02-08 18:38:53 +0530

    Added Rotary Positional Embedding

--- tests/adapters.py
+++ tests/adapters.py
@@ -206,7 +206,9 @@ def run_rope(
     Returns:
         Float[Tensor, " ... sequence_length d_k"]: Tensor with RoPEd input.
     """
-    raise NotImplementedError
+    from cs336_basics.RotaryPositionalEmbedding import RotaryPositionalEmbedding
+    rope = RotaryPositionalEmbedding(theta, d_k, max_seq_len, device=in_query_or_key.device)
+    return rope(in_query_or_key, token_positions)
 
 
 def run_transformer_block(
